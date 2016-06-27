19f186fa888f5877bbf4c66ba5773bd39a8a95e9
Josh T <jptucker2009@gmail.com>
2016-06-27 17:03:13 -0400

    third commit

--- src/CounterApp.java
+++ src/CounterApp.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 public class CounterApp {
 	public static void main(String[]args){
-		
+		System.out.println("Press the enter key to stop the timer");
+		Thread counter = new Thread(new Counter());
+		counter.start();
+		Scanner keyboard = new Scanner(System.in);
 	}
 }
